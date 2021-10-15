6e5a48ec720fc0062a9532514fcca774cfc27346
Michael Huang <michaelghuang19@gmail.com>
2021-10-15 11:00:31 -0700

    problem 1 draft

--- hw2.R
+++ hw2.R
@@ -3,7 +3,17 @@
 # Homework 2
 
 # 1(a)
+a_vector <- dpois(c(3,4,5), 2)
+sum(a_vector)
 
+# 1(b)
+pnorm(-4)
+
+# 1(c)
+# check and see if this is right for sd_nu adjustment or not
+# sd_nu <- sqrt(10 / (10 - 2))
+# pt(-4*sd_nu, 10)
+pt(-4, 10)
 
 # 2
 
