e767d62cad0d55893d8b396106960a3ef88abf35
Michael Huang <michaelghuang19@gmail.com>
2021-11-15 17:04:27 -0800

    q3 complete

--- hw5/hw5.R
+++ hw5/hw5.R
@@ -10,14 +10,9 @@ sample <- "1998-01::2012-05"
 sp500.daily.price <- sp500DailyPrices[sample]
 sp500.daily.ret <- na.omit(Return.calculate(sp500.daily.price, method="log"))
 
-sp500.yearly.ret <- c()
-for (year in 1998:2012) {
-  sp500.yearly.ret <- append(sp500.yearly.red, sum(sp500.daily.ret[as.character(year)]))
-}
-
-sp500_ret_mean <- mean(sp500.yearly.ret)
-sp500_ret_var <- var(sp500.yearly.ret)
-sp500_ret_sd <- sd(sp500.yearly.ret)
+sp500_ret_mean <- mean(sp500.daily.ret)
+sp500_ret_var <- var(sp500.daily.ret)
+sp500_ret_sd <- sd(sp500.daily.ret)
 
 alpha <- 0.05
 n <- 252
@@ -26,7 +21,8 @@ n <- 252
 set.seed(123)
 
 r <- rnorm(n=n, mean=sp500_ret_mean, sd=sp500_ret_sd)
-ci <- sp500_ret_mean + c(-1,1)*qnorm(1-alpha/2)*sp500_ret_sd/sqrt(length(sp500.yearly.ret))
+ci <- sp500_ret_mean + c(-1,1)*qnorm(1-alpha/2)*sp500_ret_sd/sqrt(n)
+print(252 * ci)
 
 # Question 2
 
