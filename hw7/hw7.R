332d41974d28448a5bd2b2f1b7a293aa10b83d39
Michael Huang <michaelghuang19@gmail.com>
2021-11-26 10:34:12 -0800

    "complete" 2

--- hw7/hw7.R
+++ hw7/hw7.R
@@ -21,10 +21,113 @@ t.test(x=r2, mu=mu0)
 
 # Question 2
 
+# 2(b)
+
+sims <- 10000
+n <- 100
+mu <- 0
+sigma_sq <- 1
+
+p_vals <- numeric(10000)
+
+for (i in 1:sims) {
+  x <- rnorm(n, mean=mu, sd=sigma_sq)
+  
+  test <- t.test(x)
+  p_vals[i] <- test$p.value
+}
+
+hist(p_vals, plot=TRUE)
+
+# 2(c)
+
+mu <- 0.2
+
+p_vals <- numeric(10000)
+
+for (i in 1:sims) {
+  x <- rnorm(n, mean=mu, sd=sigma_sq)
+  
+  test <- t.test(x)
+  p_vals[i] <- test$p.value
+}
+
+hist(p_vals, plot=TRUE)
+
+# 2(d)
+
+mu <- 0.05
+
+p_vals <- numeric(10000)
+
+for (i in 1:sims) {
+  x <- rnorm(n, mean=mu, sd=sigma_sq)
+  
+  test <- t.test(x)
+  p_vals[i] <- test$p.value
+}
+
+hist(p_vals, plot=TRUE)
+
+# 2(e)
+
+mu <- 0.05
+n <- 200
+
+p_vals <- numeric(10000)
+
+for (i in 1:sims) {
+  x <- rnorm(n, mean=mu, sd=sigma_sq)
+  
+  test <- t.test(x)
+  p_vals[i] <- test$p.value
+}
+
+hist(p_vals, plot=TRUE)
+
+# 2(f)
+
 library(IntroCompFinR)
 library(PerformanceAnalytics)
 sample <- "1998-01::2012-05"
 msft.daily.price <- msftDailyPrices[sample]
 msft.daily.ret <- na.omit(Return.calculate(msft.daily.price, method="log"))
+
+ret <- coredata(msft.daily.ret)
+ret.mean <- mean(ret)
+ret.sd <- sd(ret)
+
+ret <- ret - ret.mean
+ret <- ret / ret.sd
+
+n <- 100
+
+p_vals <- numeric(10000)
+
+for (i in 1:sims) {
+  x <- sample(ret, n, replace=TRUE)
+  x <- coredata(x)
+  
+  test <- t.test(x)
+  p_vals[i] <- test$p.value
+}
+
+hist(p_vals, plot=TRUE)
+
+# 2(g)
+
+ret <- ret + 0.05
+
+p_vals <- numeric(10000)
+
+for (i in 1:sims) {
+  x <- sample(ret, n, replace=TRUE)
+  
+  test <- t.test(x)
+  p_vals[i] <- test$p.value
+}
+
+hist(p_vals, plot=TRUE)
+
 
 
