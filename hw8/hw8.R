4ef7ca6074439288ccdbd3057b2aa859f58acd19
Michael Huang <michaelghuang19@gmail.com>
2021-12-04 20:06:07 -0800

    1 draft

--- hw8/hw8.R
+++ hw8/hw8.R
@@ -4,11 +4,20 @@
 
 # Question 1
 
+# 1(a)
 
+sol <- polyroot(c(1, 0.5))
+Mod(sol)
+
+# 1(b)
+
+sol <- polyroot(c(1, -1.5, 0.5))
+Mod(sol)
 
 # Question 2
 
-
+sol <- polyroot(c())
+Mod(sol)
 
 # Question 3
 
